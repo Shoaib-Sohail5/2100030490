6133e63f60dd223ad7235e0885c2ae6680f45e36
Shoaib Sohail <136471399+Shoaib-Sohail5@users.noreply.github.com>
2024-06-13 12:54:06 +0530

    Delete myApp directory

--- myApp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Numbers(models.Model):
-    value=models.IntegerField(unique=True)
-    created_at=models.DateTimeField(auto_now_add=True)
--- myApp/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from . models import Numbers
-
-class numberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Numbers
-        fields=['id','value']
-    
